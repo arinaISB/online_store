f77cf131acbc3cc646346a8120c2018666ee77b7
aidamsyuryun <aidamsyuryun@team.amocrm.com>
2025-03-28 01:21:13 +0300

    test migration entity

---
+++ src/Entity/Report.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use DateTimeImmutable;
+use Doctrine\ORM\Mapping as ORM;
+
+class Report
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'CUSTOM')]
+    #[ORM\Column(type: 'uuid')]
+    private string $uuid;
+
+    #[ORM\Column(length: 20)]
+    private string $status;
+
+    #[ORM\Column(length: 255)]
+    private string $filePath;
+
+    #[ORM\Column]
+    private DateTimeImmutable $createdAt;
+
+    public function getUuid(): int
+    {
+        return $this->uuid;
+    }
+
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getFilePath(): string
+    {
+        return $this->filePath;
+    }
+
+    public function setFilePath(string $filePath): static
+    {
+        $this->filePath = $filePath;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
