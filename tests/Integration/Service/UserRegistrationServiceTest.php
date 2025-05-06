3c55dc14a5165fe0c20016ac7ee40becc6eb541a
aidamsyuryun <aidamsyuryun@team.amocrm.com>
2025-05-07 02:00:45 +0300

    тесты

--- tests/Integration/Service/UserRegistrationServiceTest.php
+++ tests/Integration/Service/UserRegistrationServiceTest.php
@@ -6,6 +6,7 @@ namespace Integration\Service;
 
 use App\Dto\UserRegistrationDto;
 use App\Entity\User;
+use App\Exception\UserAlreadyExistsException;
 use App\Service\UserRegistrationService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -13,32 +14,47 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 class UserRegistrationServiceTest extends WebTestCase
 {
     private UserRegistrationService $service;
-    private UserRegistrationDto $dto;
 
     protected function setUp(): void
     {
         self::bootKernel();
 
         $this->service = self::getContainer()->get(UserRegistrationService::class);
+    }
 
-        $this->dto = new UserRegistrationDto(
+    public function testRegister(): void
+    {
+        $dto = new UserRegistrationDto(
             'Arina',
             'arina@gmail.com',
             '+1234567890',
             'secure123',
             'secure123'
         );
-    }
 
-    public function testRegister(): void
-    {
-        $jwt = $this->service->register($this->dto);
+        $jwt = $this->service->register($dto);
 
         $this->assertNotEmpty($jwt);
         $this->assertNotNull(
             self::getContainer()->get(EntityManagerInterface::class)
                 ->getRepository(User::class)
-                ->findOneBy(['email' => $this->dto->email])
+                ->findOneBy(['email' => $dto->email])
         );
+    }
+
+    public function testRegisterWithExistingEmail(): void
+    {
+        $dto = new UserRegistrationDto(
+            'John',
+            'existingEmail@mail.ru',
+            '+1234567890',
+            'secure123',
+            'secure123'
+        );
+
+        $this->service->register($dto);
+
+        $this->expectException(UserAlreadyExistsException::class);
+        $this->service->register($dto);
     }
 }
